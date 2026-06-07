c7e6817f621645791780d5bc3717c296ed3ce614
OliveiraMatheuus <mojesus@unifesp.br>
2026-06-07 20:25:44 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho_POO/EntidadeJogo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_POO
+{
+    internal abstract class EntidadeJogo
+    {
+        public PictureBox Sprite { get; protected set; }
+        public int Velocidade { get; protected set; }
+        public int Vidas { get; set; }
+        public abstract void Mover();
+    }
+
+    public interface Destruivel
+    {
+        void ReceberDano();
+        void Destruir();
+    }
+}
---
+++ Trabalho_POO/Nave.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Trabalho_POO
+{
+    public delegate void EntidadeDestruidaHandler();
+
+    internal class NaveJogador : EntidadeJogo, Destruivel
+    {
+        public event EntidadeDestruidaHandler Destruida;
+
+        // --- CONSTRUTOR ---
+        public NaveJogador(PictureBox spriteNave)
+        {
+            // Atribuindo valores às propriedades herdadas de EntidadeJogo
+
+            Sprite = spriteNave;
+            Velocidade = 1;
+            Vidas = 3;
+        }
+
+        public void ReceberDano()
+        {
+            Vidas--;
+
+            
+            if (Vidas <= 0)
+            {
+                Destruir();
+            }
+        }
+
+        public void Destruir()
+        {
+            
+            Destruida?.Invoke();
+            Sprite.Dispose();
+        }
+
+       
+
+        public override void Mover(A)
+        {
+            
+        }
+
+
+        public void MoverEsquerda()
+        {
+            
+            if (Sprite.Left > 0)
+            {
+                Sprite.Left -= Velocidade;
+            }
+        }
+
+        public void MoverDireita(int larguraTela)
+        {
+            
+            if (Sprite.Right < larguraTela)
+            {
+                Sprite.Left += Velocidade;
+            }
+        }
+    }
+}
